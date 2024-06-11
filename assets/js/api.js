1e191f8c94af0447aa5c35091040ef08354c6c70
phg02 <s4063236@rmit.edu.vn>
2024-06-11 15:22:51 +0700

    bug

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,40 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+const api_key = "2e53eec10ac3a47083594e6b29791954";
+/**
+ * Fetch data from api
+ * @param {string} URL 
+ * @param {function} callback 
+ */
+
+
+
+export const fetchData = function(URL, callback){
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data =>  callback(data))
+}
+
+export const url ={
+    currentWeather(lat, long){
+        return`https://api.openweathermap.org/data/2.5/weather?${lat}&${long}&units=metric`
+    },
+
+    forecast(lat, long){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${long}&units=metric`
+    },
+    airPollution(lat, long){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${long}`;
+    },
+    reverseGeo(lat, long){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${long}&limit=5`
+    },
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}}&limit=5`
+    }
+
+}
--- assets/js/route.js
+++ assets/js/route.js
@@ -4,3 +4,42 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+'use strict';
+
+import {updateWeather, error404} from './app.js';
+
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474";
+
+
+const currentLocation = function(){
+    window.navigator.geolocation.getCurrentPosition(position => {
+        const {latitude, longitude} = position.coords;
+        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    }, err => {window.location.hash = defaultLocation})
+}
+
+const searchLocation = query => {
+    updateWeather(...query.split('&'));
+}
+
+const routes = new Map([
+    ["/current-location", currentLocation],
+    ["/weather", searchLocation]
+])
+
+const checkHash = function(){
+    const requestURL = window.location.hash.slice(1);
+    const [route, query] = requestURL.includes ? requestURL.split('?'): [requestURL];
+    routes.get(route) ? routes.get(route)(query) : error404();
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function(){
+    if(!this.window.location.hash === ""){
+        this.window.location.hash = "#/current-location";
+    } else{
+        checkHash();
+    }
+});
+
